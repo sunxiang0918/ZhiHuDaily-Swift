4041fee1dd50c7c718231da0510030126f78436c
SUN <sunxiang0918@gmail.com>
2015-05-26 18:04:25 +0800

    第一次初始化项目

---
+++ ZhiHuDaily-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiHuDaily-Swift
+//
+//  Created by SUN on 15/5/26.
+//  Copyright (c) 2015年 SUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
