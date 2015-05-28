47692dd071dd3229f949836eec955c64bfaeaa7f
SUN <sunxiang0918@gmail.com>
2015-05-28 21:47:56 +0800

    使用tableView来实现了上划透明的效果

--- ZhiHuDaily-Swift/ViewController.swift
+++ ZhiHuDaily-Swift/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     var leftViewController : UIViewController?
     
+    let scrollHeight:Float = 80
     let kImageHeight:Float = 400
     let kInWindowHeight:Float = 200
+    let titleHeight:Float = 44
     
     //主页面上关联的表格
     @IBOutlet weak var mainTableView: UITableView!
+    @IBOutlet weak var titleView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,36 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //整个View的上下滑动事件的响应
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        if  scrollView is UITableView {
+            //只有是在上下滑动TableView的时候进行处理
+            changeTitleViewAlpha(Float(scrollView.contentOffset.y))
+            
+        }
+    }
+    
+    //这部分是用来根据TableView的滑动来调整TitleView的透明度的
+    func changeTitleViewAlpha(offsetY:Float){
+        //计算出最大上划大小. 当上划到此处后, title就全部显示
+        let needY=kInWindowHeight-scrollHeight-titleHeight
+        
+        //计算出透明度
+        var result =  offsetY/needY
+        
+        if result>1{
+            result = 1.0
+        }else if result<0 {
+            result = 0.0
+        }
+        
+        //这里使用的是修改他得背景颜色的透明度来实现的.不直接使用titleView.alpha = CGFloat(result)是因为, 这样修改会导致这个View上面的所有的subView都会透明
+        titleView.backgroundColor = UIColor(red: 0.125, green: 0.471, blue: 1.000, alpha: CGFloat(result))
 
+    }
+    
     //================UITableViewDataSource的实现================================
     
     //设置tableView的数据行数
