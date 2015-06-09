aea76536227ab439e041526981e1486630d89705
SUN <sunxiang0918@gmail.com>
2015-06-09 23:47:30 +0800

    增加了 评论详细页面

---
+++ ZhiHuDaily-Swift/CommonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CommonViewController.swift
+//  ZhiHuDaily-Swift
+//
+//  Created by SUN on 15/6/9.
+//  Copyright (c) 2015年 SUN. All rights reserved.
+//
+
+import UIKit
+
+class CommonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
