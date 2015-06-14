6b6c398d16454533e10ad24c85cf949d626bf369
SUN <sunxiang0918@gmail.com>
2015-06-14 17:06:01 +0800

    增加了 发布评论的 页面.  但是不打算写  功能的实现了

---
+++ ZhiHuDaily-Swift/WriteCommentViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WriteCommentViewController.swift
+//  ZhiHuDaily-Swift
+//
+//  Created by SUN on 15/6/14.
+//  Copyright (c) 2015年 SUN. All rights reserved.
+//
+
+import UIKit
+
+class WriteCommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doBackAction(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in
+            
+        })
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
